17d9b50c2063669c34d294f79ba0c19d4a270426
Mclara08 <mclarapss@gmail.com>
2021-04-23 16:35:15 -0300

    Master atualizada

--- atestado/settings.py
+++ atestado/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_atestado',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': '12345',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
