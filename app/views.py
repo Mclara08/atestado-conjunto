dfa07ed82995ee24709bf995e934d17d777631b4
Mclara08 <mclarapss@gmail.com>
2021-05-20 11:27:30 -0300

    Pesquisa com checkbox finalizada

--- app/views.py
+++ app/views.py
@@ -10,9 +10,11 @@ from django.db.models import Q
 from django.views.decorators.csrf import csrf_protect
 from django.core.paginator import Paginator
 
+
 # Create your views here.
 def entrar(request):
     return render(request, 'login.html')
+
 
 @csrf_protect
 def submit(request):
@@ -27,22 +29,24 @@ def submit(request):
             messages.error(request, 'Usuário e senha não coincidem')
             return redirect('entrar')
 
+
 def sair(request):
     logout(request)
     return redirect('/entrar')
 
-#home = referente ao index
+
+# home = referente ao index
 @login_required(login_url='/entrar')
 def home(request):
     if request.user.is_authenticated:
-        data = {}
-        data['db'] = Atestados.objects.all()
+        data = {'db': Atestados.objects.all()}
         return render(request, 'index.html', data)
     else:
         messages.error(request, 'Usuário não conectado!')
         return redirect('login.html')
 
-#pesquisa = referente à página de pesquisa
+
+# pesquisa = referente à página de pesquisa
 def pesquisa(request):
     if request.user.is_authenticated:
         data = {}
@@ -55,33 +59,41 @@ def pesquisa(request):
         busca_data_emissao2 = request.GET.get('busca_data_emissao2')
 
         # Tipo de serviço
-        lista_servicos = []
+        lista_servicos = Q()
         busca_servico_desenv = request.GET.get('busca_servico_desenv')
         busca_servico_its = request.GET.get('busca_servico_its')
         busca_servico_sd = request.GET.get('busca_servico_sd')
         busca_servico_sup = request.GET.get('busca_servico_sup')
         if busca_servico_desenv:
-            lista_servicos.append(busca_servico_desenv)
+            lista_servicos.add(Q(tipo_de_servico=busca_servico_desenv), Q.OR)
         if busca_servico_its:
-            lista_servicos.append(busca_servico_its)
+            lista_servicos.add(Q(tipo_de_servico=busca_servico_its), Q.OR)
         if busca_servico_sd:
-            lista_servicos.append(busca_servico_sd)
+            lista_servicos.add(Q(tipo_de_servico=busca_servico_sd), Q.OR)
         if busca_servico_sup:
-            lista_servicos.append(busca_servico_sup)
+            lista_servicos.add(Q(tipo_de_servico=busca_servico_sup), Q.OR)
 
         # Cliente
-        busca_cliente1 = request.GET.get('busca_cliente_1')
-        busca_cliente2 = request.GET.get('busca_cliente_2')
-        busca_cliente3 = request.GET.get('busca_cliente_3')
-        busca_cliente4 = request.GET.get('busca_cliente_4')
+        lista_cliente = Q()
+        i = 0
+        while i <= Cliente.objects.all().count():
+            if request.GET.get('busca_cliente_' + str(i)):
+                busca_cliente = request.GET.get('busca_cliente_' + str(i))
+                lista_cliente.add(Q(cliente__id=busca_cliente), Q.OR)
+            i += 1
 
         # Empresa
-        busca_empresa1 = request.GET.get('busca_empresa_1')
-        busca_empresa2 = request.GET.get('busca_empresa_2')
-        busca_empresa3 = request.GET.get('busca_empresa_3')
+        lista_empresa = Q()
+        j = 0
+        while j <= Empresa.objects.all().count():
+            if request.GET.get('busca_empresa_' + str(j)):
+                busca_empresa = request.GET.get('busca_empresa_' + str(j))
+                lista_empresa.add(Q(empresa__id=busca_empresa), Q.OR)
+            j += 1
 
         lista_pesquisa = Q(id__gt=0)
 
+        # Adicionando atributos na lista para filtrar
         if busca_numero:
             lista_pesquisa.add(Q(numero_documento=busca_numero), Q.AND)
         if busca_data_emissao1 and busca_data_emissao2:
@@ -91,54 +103,39 @@ def pesquisa(request):
                 lista_pesquisa.add(Q(data_emissao=busca_data_emissao1), Q.AND)
             else:
                 lista_pesquisa.add(Q(data_emissao=busca_data_emissao2), Q.AND)
+        if lista_servicos:
+            lista_pesquisa.add(lista_servicos, Q.AND)
+        if lista_cliente:
+            lista_pesquisa.add(lista_cliente, Q.AND)
+        if lista_empresa:
+            lista_pesquisa.add(lista_empresa, Q.AND)
 
-        for item in lista_servicos:
-            i = lista_servicos.__len__() - 1
-            if item != lista_servicos[i]:
-                lista_pesquisa.add(Q(tipo_de_servico=item), Q.OR)
-            else:
-                lista_pesquisa.add(Q(tipo_de_servico=item), Q.AND)
-
-        if busca_cliente1:
-            lista_pesquisa.add(Q(cliente__id=busca_cliente1), Q.AND)
-        if busca_cliente2:
-            lista_pesquisa.add(Q(cliente__id=busca_cliente2), Q.AND)
-        if busca_cliente3:
-            lista_pesquisa.add(Q(cliente__id=busca_cliente3), Q.AND)
-        if busca_cliente4:
-            lista_pesquisa.add(Q(cliente__id=busca_cliente4), Q.AND)
-
-        if busca_empresa1:
-            lista_pesquisa.add(Q(empresa__id=busca_empresa1), Q.AND)
-        if busca_empresa2:
-            lista_pesquisa.add(Q(empresa__id=busca_empresa2), Q.AND)
-        if busca_empresa3:
-            lista_pesquisa.add(Q(empresa__id=busca_empresa3), Q.AND)
-
-        if lista_pesquisa != []:
+        # Verifica existência da lista e retorna os resultados em páginas
+        if lista_pesquisa:
             lista = Atestados.objects.filter(lista_pesquisa)
-            data['clientes'] = Cliente.objects.all();
-            data['empresas'] = Empresa.objects.all();
+            data['clientes'] = Cliente.objects.all()
+            data['empresas'] = Empresa.objects.all()
             if lista:
                 paginator = Paginator(lista, 6)
                 num_pag = request.GET.get('page')
                 data['paginas'] = paginator.get_page(num_pag)
                 return render(request, 'pesquisa.html', data)
             else:
-                messages.error(request, 'Sistema não contém nenhum registro com essas especificações!')
+                messages.error(request, 'Sistema não contém nenhum registro com essa(s) especificação(s)!')
                 return redirect('pesquisa')
     else:
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
 
+
 def form(request):
     if request.user.is_authenticated:
-        data = {}
-        data['form'] = AtestadosForm()
+        data = {'form': AtestadosForm()}
         return render(request, 'atestado_form.html', data)
     else:
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
+
 
 # Função para cadastrar atestados
 def create(request):
@@ -146,21 +143,27 @@ def create(request):
         form = AtestadosForm(request.POST, request.FILES or None)
         if form.is_valid():
             if form.cleaned_data['data_emissao'] > datetime.date.today():
-                messages.error(request, 'Operação não pôde ser realizada! A data de emissão deve ser igual ou inferior à data do dia de hoje.')
+                messages.error(request,
+                               'Operação não pôde ser realizada! A data de emissão deve ser igual ou inferior à data do dia de hoje.')
                 return redirect('form')
             else:
-                Atestados.objects.create(numero_documento=form.cleaned_data['numero_documento'], tipo_de_servico=form.cleaned_data['tipo_de_servico'],
-                                         data_emissao=form.cleaned_data['data_emissao'], empresa=form.cleaned_data['empresa'],
-                                         cliente=form.cleaned_data['cliente'], documento_pdf=form.cleaned_data['documento_pdf'],
+                Atestados.objects.create(numero_documento=form.cleaned_data['numero_documento'],
+                                         tipo_de_servico=form.cleaned_data['tipo_de_servico'],
+                                         data_emissao=form.cleaned_data['data_emissao'],
+                                         empresa=form.cleaned_data['empresa'],
+                                         cliente=form.cleaned_data['cliente'],
+                                         documento_pdf=form.cleaned_data['documento_pdf'],
                                          created_by=request.user)
                 messages.success(request, 'Operação realizada com sucesso!')
                 return redirect('form')
         else:
-            messages.error(request, 'Operação não pôde ser realizada! Por favor, verifique se o número do documento informado já existe na base de dados.')
+            messages.error(request,
+                           'Operação não pôde ser realizada! Por favor, verifique se o número do documento informado já existe na base de dados.')
             return redirect('form')
     else:
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
+
 
 # Função para visualizar detalhes de atestados cadastrados
 def view(request, pk):
@@ -172,7 +175,8 @@ def view(request, pk):
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
 
-#searchall = pesquisa todos os campos
+
+# searchall = pesquisa todos os campos
 def searchall(request):
     if request.user.is_authenticated:
         data = {}
@@ -184,6 +188,7 @@ def searchall(request):
     else:
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
+
 
 # Função para tela de edição de atestados
 def edit(request, pk):
@@ -198,6 +203,7 @@ def edit(request, pk):
     else:
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
+
 
 # Função para atualizar edições de atestados
 def update(request, pk):
@@ -218,6 +224,7 @@ def update(request, pk):
         messages.error(request, 'Usuário não conectado!')
         return redirect('entrar')
 
+
 # Função para deletar atestados
 def delete(request, pk):
     if request.user.is_authenticated:
@@ -227,7 +234,8 @@ def delete(request, pk):
             db.delete()
             return redirect('pesquisa')
         else:
-            messages.error(request, 'O atestado selecionado não pertence ao usuário atual, portanto, este não está autenticado para realizar a ação!')
+            messages.error(request,
+                           'O atestado selecionado não pertence ao usuário atual, portanto, este não está autenticado para realizar a ação!')
             return redirect('pesquisa')
     else:
         messages.error(request, 'Usuário não conectado!')
